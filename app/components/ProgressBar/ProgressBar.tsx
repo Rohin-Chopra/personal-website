336425099cea81e3ceed5261cbd67df117e9de9a
Rohin Chopra <rohinchopra1212@gmail.com>
2023-09-05 03:48:00 +1000

    cleanup

--- app/components/ProgressBar/ProgressBar.tsx
+++ app/components/ProgressBar/ProgressBar.tsx
@@ -18,7 +18,9 @@ export const ProgressBar = () => {
 
     window.addEventListener("scroll", onScroll);
 
-    return () => window.removeEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
   }, []);
 
   return (
--- app/components/ui/progress.tsx
+++ app/components/ui/progress.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import * as React from "react";
 import * as ProgressPrimitive from "@radix-ui/react-progress";
+import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
@@ -18,7 +18,7 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-slate-900 transition-all dark:bg-slate-50"
+      className="h-full w-full flex-1 bg-slate-900 dark:bg-slate-50"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
